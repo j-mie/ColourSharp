5063c8f04ba505ff6d25bf0a22b456dea679c3c7
Ben Cox <ben@benjojo.co.uk>
2014-03-18 18:30:22 +0000

    Improved the speed of the arduio code

--- Arduino Sketch/sketch_dec15a.ino
+++ Arduino Sketch/sketch_dec15a.ino
@@ -1,7 +1,3 @@
-byte incomingByte = 0;
-int datar = 0;
-int datag = 0;
-int datab = 0;
 int part = 0;
 byte numbertrans = 0;
 
@@ -12,21 +8,16 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) { 
     numbertrans = Serial.read();
-    
-    if (part == 0)
-      datar = numbertrans;
-    if (part == 1)
-      datag = numbertrans;
-    if (part == 2)
-      datab = numbertrans;
 
-    part++;
-  //Serial.println(numbertrans);     
-    if (part == 3) {       
-      analogWrite(9, datar); // Pin 9 Needs to be the Red LED
-      analogWrite(10, datag); // Pin 10 Needs to be the Green LED
-      analogWrite(11, datab); // Pin 11 Needs to be the Blue LED
+    if (part == 0)
+      analogWrite(9, numbertrans); // Pin 9 Needs to be the Red LED
+    if (part == 1)
+      analogWrite(10, numbertrans); // Pin 10 Needs to be the Green LED
+    if (part == 2)
+      analogWrite(11, numbertrans); // Pin 11 Needs to be the Blue LED
+
+    part++;     
+    if (part == 3)
       part = 0;
-    }
   }
 }
